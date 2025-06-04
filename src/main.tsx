cba60bb5808fd84068e422bd73f20e10f8584491
VizzDum <attorney.kucher@gmail.com>
2025-06-04 16:12:29 +0300

    Updates

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Symbols from './pages/Symbols'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/symbols" element={<Symbols />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
